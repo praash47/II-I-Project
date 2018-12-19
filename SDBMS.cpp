e7e0eea79ce72d8582ff5e413f19b5d6dac7b997
unknown <praash47@gmail.com>
2018-12-19 18:16:15 +0545

    Changes some other things.

--- SDBMS.cpp
+++ SDBMS.cpp
@@ -1,6 +1,6 @@
 #include<iostream.h>
 #include<conio.h>
-#include<graphics.h> // For drawing graphics
+#include<graphics.h> // For drawing graphics & mouse controls
 #include<dos.h> // For getting date
 
 class user
@@ -15,37 +15,36 @@ private:
     char name[30]; // 30 characters full name
 };
 
-/* Mouse Showing Functions(M) */
+/* Mouse Functions(M) */
+int gd = DETECT, gm; // graphic variables
 union REGS i,o; // Mouse input & output controller
 
 int initmouse() // Mouse initialization(M)
 {
     i.x.ax = 0;
-    int86(0X33, &i, &o);
+    int86(0x33, &i, &o);
     return(o.x.ax);
 }
 void showmouseptr() // Mouse pointer show(M)
 {
     i.x.ax = 1;
-    int86(0X33, &i, &o);
+    int86(0x33, &i, &o);
 }
 
-void restrictmouse(int x1, int y1, int x2, int y2) // Mouse restriction in screen
+void restrictmouse(int x1, int y1, int x2, int y2) // Mouse restriction in screen (M)
 {
     i.x.ax = 7; // Top left boundary
     i.x.cx = x1;
     i.x.dx = x2;
-    int86(0X33, &i, &o);
+    int86(0x33, &i, &o);
     i.x.ax = 8; // Bottom right boundary
     i.x.cx = y1;
     i.x.dx = y2;
-    int86(0X33, &i, &o);
+    int86(0x33, &i, &o);
 }
 
 void main()
 {
-    int gd = DETECT, gm; // graphic variables
-
     initgraph(&gd,&gm,"C:\\TC\\BGI"); // Loads driver for graphics
 
     showmouseptr(); // This shows mouse!(M)
